40c6f90f12a8c57cc1b394980a31635184f87128
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-11-02 09:54:53 -0700

    11 - Add ajax to return json of cats toys when cat is clicked

--- p3/app/assets/javascripts/toys.js
+++ p3/app/assets/javascripts/toys.js
@@ -1,1 +1,10 @@
 //created by hand at 9:43AM. did not generate
+$(document).ready(function(){
+  showToyListener();
+})
+
+var showToyListener = function(){
+  $('.link').on('ajax:success', function(event, response){
+    console.log(response);
+  })
+}
--- p3/app/controllers/toys_controller.rb
+++ p3/app/controllers/toys_controller.rb
@@ -2,6 +2,11 @@ class ToysController < ApplicationController
   def index
     @pet = Pet.find(params[:pet_id])
     @toys = @pet.toys
+    if request.xhr?
+      render json: @toys
+    else
+      render 'index'
+    end
   end
 
   def show
