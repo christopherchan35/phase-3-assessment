c7920b8df0eaf645acf353017ad7ea42ca9bc937
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-11-02 09:57:31 -0700

    12 - Change home to /pets with custom route

--- p3/config/routes.rb
+++ p3/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # HAVING DIFFICULTY WITH CUSTOM ROUTES, I WANT TO SET ROOT TO /PETS INSTEAD OF / AND DISPLAY ALL PETS
-  root 'pets#index'
-  # root :to => 'pets#index'
+  # root 'pets#index'
+  root :to => redirect('/pets')
   # get '/', :to => 'pets#index'
   get '/pets', :to => 'pets#index'
 
